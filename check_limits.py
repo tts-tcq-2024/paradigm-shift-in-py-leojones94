94dd4b67b1478150d29b5c4c9840f68f14bf9662
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-07-02 11:32:55 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,24 +1,26 @@
-def is_Temperature_Ok(temperature):
-    if temperature < 0 or temperature > 45:
-        print("Temperature out of range!")
-        return False
-    return True
+def temp_checker(temp):
+  t = range(0,45)
+  if (temp not in t):
+    print ("Temperature out of range")
+    return False
+  return True
 
-def is_Soc_Ok(soc):
-    if soc < 20 or soc > 80:
-        print("State of Charge out of range!")
-        return False
-    return True
+def soc_checker(soc):
+  s = range(20,80)
+  if (soc not in s):
+    print ("State of Charge out of range")
+    return False
+  return True
+  
+def charge_rate_checker(cr):
+  if cr > 0.8:
+    print("Charge Rate out of range")
+    return False
+  return True
 
-def is_ChargeRate_Ok(chargeRate):
-    if chargeRate > 0.8:
-        print("Charge Rate out of range!")
-        return False
-    return True
-
-def battery_Is_Ok(temperature, soc, chargeRate):
-    return is_Temperature_Ok(temperature) and is_Soc_Ok(soc) and is_ChargeRate_Ok(chargeRate)
+def battery_is_ok(temperature, soc, charge_rate):
+  return (temp_checker and soc_checker and charge_rate_checker)
 
 if __name__ == '__main__':
-    assert battery_Is_Ok(25, 70, 0.7) == True
-    assert battery_Is_Ok(50, 85, 0) == False
+  assert(battery_is_ok(25, 70, 0.7) is True)
+  assert(battery_is_ok(50, 85, 0) is False)
