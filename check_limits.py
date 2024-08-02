4ec5a57df4bbfc8e34fb9af8f9e22116893796c3
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-02 11:13:11 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,26 +1,45 @@
-def temp_checker(temp):
-  t = range(0,45)
-  if (temp not in t):
-    print ("Temperature out of range")
-    return False
-  return True
+def translate_to_common_units(value, parameter):
+    return value
 
-def soc_checker(soc):
-  s = range(20,80)
-  if (soc not in s):
-    print ("State of Charge out of range")
-    return False
-  return True
-  
-def charge_rate_checker(cr):
-  if cr > 0.8:
-    print("Charge Rate out of range")
-    return False
-  return True
+def map_breach_to_ranges(value, parameter):
+    lower_limit = parameter['lower_limit']
+    upper_limit = parameter['upper_limit']
+    warning_tolerance = parameter['warning_tolerance']
+    
+    if value < lower_limit:
+        return 'low_breach'
+    elif value < lower_limit + (upper_limit * warning_tolerance):
+        return 'low_warning'
+    elif value > upper_limit:
+        return 'high_breach'
+    elif value > upper_limit - (upper_limit * warning_tolerance):
+        return 'high_warning'
+    else:
+        return 'normal'
+
+def translate_anomaly_to_message(anomaly, parameter):
+    messages = {
+        'low_breach': 'out of range',
+        'low_warning': 'approaching low',
+        'high_breach': 'out of range',
+        'high_warning': 'approaching high'
+    }
+    return f"{parameter['name']} {messages[anomaly]}"
+
+def check_parameter(value, parameter):
+    anomaly = map_breach_to_ranges(value, parameter)
+    message = translate_anomaly_to_message(anomaly, parameter)
+    print(message)
+    return anomaly != 'high_breach' and anomaly != 'low_breach'
 
 def battery_is_ok(temperature, soc, charge_rate):
-  return (temp_checker(temperature) and soc_checker(soc) and charge_rate_checker(charge_rate))
+    parameters = [
+        {'name': 'Temperature', 'lower_limit': 0, 'upper_limit': 45, 'warning_tolerance': 0.05},
+        {'name': 'State of Charge', 'lower_limit': 20, 'upper_limit': 80, 'warning_tolerance': 0.05},
+        {'name': 'Charge Rate', 'upper_limit': 0.8, 'warning_tolerance': 0.05}
+    ]
+    return all(check_parameter(value, parameter) for value, parameter in zip([temperature, soc, charge_rate], parameters))
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    assert(battery_is_ok(25, 70, 0.7) is True)
+    assert(battery_is_ok(50, 85, 0) is False)
