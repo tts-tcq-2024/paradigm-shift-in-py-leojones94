6bc3aa124e094bee247d0fb0757e510ae53c75bb
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-03 09:17:03 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,45 +1,123 @@
-def translate_to_common_units(value, parameter):
-    return value
-
-def map_breach_to_ranges(value, parameter):
-    lower_limit = parameter['lower_limit']
-    upper_limit = parameter['upper_limit']
-    warning_tolerance = parameter['warning_tolerance']
+HIGH_TEMP_LIMIT = 45
+LOW_TEMP_LIMIT = 0
+LOW_TEMP_WARNING_LIMIT = 2.25
+HIGH_TEMP_WARNING_LIMIT = 42.75
+HIGH_SOC_LIMIT = 80
+LOW_SOC_LIMIT = 20
+LOW_SOC_WARNING_LIMIT = 24
+HIGH_SOC_WARNING_LIMIT = 76
+HIGH_CHARGERATE_WARNING_LIMIT = 0.76
+HIGH_CHARGERATE_LIMIT = 0.8
+parameter_dict = {'Temperature' : {
+                            LOW_TEMP_WARNING_LIMIT : 'LOW_Temperature_WARNING',
+                            HIGH_TEMP_WARNING_LIMIT: 'HIGH_Temperature_WARNING',
+                            },
+                  'SOC' : {
+                            LOW_SOC_WARNING_LIMIT: 'LOW_SOC_WARNING',
+                            HIGH_SOC_WARNING_LIMIT: 'HIGH_SOC_WARNING',
+                          },
+                  'Charge Rate' : {
+                            HIGH_CHARGERATE_LIMIT : 'Charge rate out of range',
+                            HIGH_CHARGERATE_WARNING_LIMIT: 'HIGH_Charge_Rate_WARNING',
+                          }
+                  }
+def parameter_range_selector(parameter):
+  for item in parameter_dict:
+    if item == parameter:
+      parameter_warning_limit = parameter_dict[item]
+      return parameter_warning_limit
     
-    if value < lower_limit:
-        return 'low_breach'
-    elif value < lower_limit + (upper_limit * warning_tolerance):
-        return 'low_warning'
-    elif value > upper_limit:
-        return 'high_breach'
-    elif value > upper_limit - (upper_limit * warning_tolerance):
-        return 'high_warning'
-    else:
-        return 'normal'
+def warning_level_selector(parameter_warning_limit,parameter_value):
+  low_limit_value,low_warning_message = list(parameter_warning_limit.items())[0]
+  high_limit_value,high_warning_message = list(parameter_warning_limit.items())[1]
 
-def translate_anomaly_to_message(anomaly, parameter):
-    messages = {
-        'low_breach': 'out of range',
-        'low_warning': 'approaching low',
-        'high_breach': 'out of range',
-        'high_warning': 'approaching high'
-    }
-    return f"{parameter['name']} {messages[anomaly]}"
+  if parameter_value <= low_limit_value:
+    return low_warning_message
+  elif parameter_value >= high_limit_value:
+    return high_warning_message
 
-def check_parameter(value, parameter):
-    anomaly = map_breach_to_ranges(value, parameter)
-    message = translate_anomaly_to_message(anomaly, parameter)
-    print(message)
-    return anomaly != 'high_breach' and anomaly != 'low_breach'
+def temp_checker(temp,warn_param = None):
+  t = range(0,45)
+  if (temp not in t):
+    print ("Temperature out of range")
+    temp_result = False
+    return temp_result
+  temp_result = True
+  if warn_param != None:
+    parameter_limit = parameter_range_selector(warn_param)
+    warning_message = warning_level_selector(parameter_limit,temp)
+    temp_warn_result = (temp_result,warning_message)
+    return temp_warn_result
+  return temp_result
+  
+def soc_checker(soc, warn_param = None):
+  s = range(20,80)
+  if (soc not in s):
+    print ("State of Charge out of range")
+    soc_result = False
+    return soc_result
+  soc_result = True
+  if warn_param != None:
+    parameter_limit = parameter_range_selector('SOC')
+    warning_message = warning_level_selector(parameter_limit,soc)
+    soc_warn_result = (soc_result,warning_message)
+    return soc_warn_result
+  return soc_result
+  
+def charge_rate_checker(cr,warn_param = None):
+  if cr > 0.8:
+    print("Charge Rate out of range")
+    cr_result = False
+    return cr_result
+  cr_result = True
+  if warn_param !=None:
+    if cr > HIGH_CHARGERATE_WARNING_LIMIT:
+      warning_message = 'HIGH_Charge_Rate_WARNING'
+      cr_result = (cr_result,warning_message)
+    elif cr < HIGH_CHARGERATE_WARNING_LIMIT:
+      warning_message = 'Normal'
+      cr_result = (cr_result,warning_message)
+  return cr_result
 
-def battery_is_ok(temperature, soc, charge_rate):
-    parameters = [
-        {'name': 'Temperature', 'lower_limit': 0, 'upper_limit': 45, 'warning_tolerance': 0.05},
-        {'name': 'State of Charge', 'lower_limit': 20, 'upper_limit': 80, 'warning_tolerance': 0.05},
-        {'name': 'Charge Rate', 'upper_limit': 0.8, 'warning_tolerance': 0.05}
-    ]
-    return all(check_parameter(value, parameter) for value, parameter in zip([temperature, soc, charge_rate], parameters))
+def battery_is_ok(temperature, soc, charge_rate,warn_parameter=None):
+  if warn_parameter!=None:
+    if warn_parameter == 'SOC':
+      soc_result, warn_message = soc_checker(soc,warn_parameter)
+      return ((temp_checker(temperature) and soc_result and charge_rate_checker(charge_rate)),warn_message)
+    if warn_parameter == 'Temperature':
+      temp_result,warn_message = temp_checker(temperature,warn_parameter)
+      return ((temp_result and soc_checker(soc) and charge_rate_checker(charge_rate)),warn_message)
+    if warn_parameter == 'Charge Rate':
+      cr_result,warn_message = charge_rate_checker(charge_rate,warn_parameter)
+      return ((temp_checker(temperature) and soc_checker(soc) and cr_result),warn_message)
+  return ((temp_checker(temperature) and soc_checker(soc) and charge_rate_checker(charge_rate)))
+
+import unittest
+
+class TestParadigm(unittest.TestCase):
+    
+    def test_high_temp(self):
+        self.assertEqual((battery_is_ok(44, 25, 0.7,'Temperature')),(True,'HIGH_Temperature_WARNING'))
+    
+    def test_low_temp(self):
+        self.assertEqual((battery_is_ok(1, 25, 0.7,'Temperature')),(True,'LOW_Temperature_WARNING'))
+    
+    def test_high_soc(self):
+        self.assertEqual((battery_is_ok(25, 77, 0.7,'SOC')),(True,'HIGH_SOC_WARNING'))
+    
+    def test_low_soc(self):
+        self.assertEqual((battery_is_ok(25, 21, 0.7,'SOC')),(True,'LOW_SOC_WARNING'))
+
+    def test_high_cr(self):
+        self.assertEqual((battery_is_ok(30, 30, 0.78,'Charge Rate')),(True,'HIGH_Charge_Rate_WARNING'))
+
+    def test_low_cr(self):
+        self.assertEqual((battery_is_ok(30, 30, 0,'Charge Rate')),(True,'Normal'))
+
+    def test_out_of_range(self):
+      self.assertEqual(battery_is_ok(50, 85, 0), False)
+
+    
 
 if __name__ == '__main__':
-    assert(battery_is_ok(25, 70, 0.7) is True)
-    assert(battery_is_ok(50, 85, 0) is False)
+  unittest.main()
