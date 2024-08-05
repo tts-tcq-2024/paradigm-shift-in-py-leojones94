a3261c046ffce79d774d20c9c4f2bfc210e922df
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-05 12:21:18 +0530

    Update state_of_charge_checker.py

--- state_of_charge_checker.py
+++ state_of_charge_checker.py
@@ -1,11 +1,11 @@
 from warning_check import warn_checker
 
 def soc_checker(soc,warn_param = None):
-  s = range(20,80)
-  if (soc not in s):
+  soc_result, soc_message = warn_checker(soc, "SOC", warn_param)
+  soc_accepted_range = range(20,80)
+  if (soc not in soc_accepted_range):
     soc_breach_message = "SOC out of range"
     print (soc_breach_message)
     soc_result = False
     return soc_result,soc_breach_message
-  soc_result, soc_message = warn_checker(soc, "SOC", warn_param)
   return soc_result,soc_message
