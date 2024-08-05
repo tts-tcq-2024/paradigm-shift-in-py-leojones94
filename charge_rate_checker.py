312014fc8163d8d9d62c13c73b57290e585564c2
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-05 12:29:22 +0530

    Update charge_rate_checker.py

--- charge_rate_checker.py
+++ charge_rate_checker.py
@@ -6,5 +6,5 @@ def charge_rate_checker(charge_rate,warn_param = None):
     print(charge_rate_breach_message)
     charge_rate_result = False
     return charge_rate_result,charge_rate_breach_message
-  charge_rate_result,charge_rate_breach_message = warn_checker(cr,"Charge Rate",warn_param)
+  charge_rate_result,charge_rate_breach_message = warn_checker(charge_rate,"Charge Rate",warn_param)
   return charge_rate_result,charge_rate_breach_message
