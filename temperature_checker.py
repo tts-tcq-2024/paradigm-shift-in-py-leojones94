714711b1f0f06a96aa9e81014b6d38b74f149a1a
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-05 12:20:34 +0530

    Update temperature_checker.py

--- temperature_checker.py
+++ temperature_checker.py
@@ -1,11 +1,11 @@
 from warning_check import warn_checker
 
 def temp_checker(temp,warn_param = None):
-  t = range(0,45)
-  if (temp not in t):
+  temp_result, temp_message = warn_checker(temp,"Temperature",warn_param)
+  temp_accepted_range = range(0,45)
+  if (temp not in temp_accepted_range):
     temp_breach_message = "Temperature out of range"
     print (temp_breach_message)
     temp_result = False
     return temp_result,temp_breach_message
-  temp_result, temp_message = warn_checker(temp,"Temperature",warn_param)
   return temp_result,temp_message
