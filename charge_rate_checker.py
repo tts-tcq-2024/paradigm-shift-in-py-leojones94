9c90df2aa025610da73b41643bf40045913a6af6
leojones94 <117976870+leojones94@users.noreply.github.com>
2024-08-05 12:23:23 +0530

    Update charge_rate_checker.py

--- charge_rate_checker.py
+++ charge_rate_checker.py
@@ -1,10 +1,10 @@
 from warning_check import warn_checker
 
-def charge_rate_checker(cr,warn_param = None):
-  if cr > 0.8:
-    cr_breach_message = "Charge Rate out of Range"
-    print(cr_breach_message)
-    cr_result = False
-    return cr_result,cr_breach_message
-  cr_result,cr_message = warn_checker(cr,"Charge Rate",warn_param)
-  return cr_result,cr_message
+def charge_rate_checker(charge_rate,warn_param = None):
+  if charge_rate > 0.8:
+    charge_rate_breach_message = "Charge Rate out of Range"
+    print(charge_rate_breach_message)
+    charge_rate_result = False
+    return charge_rate_result,charge_rate_breach_message
+  charge_rate_result,charge_rate_breach_message = warn_checker(cr,"Charge Rate",warn_param)
+  return charge_rate_result,charge_rate_breach_message
